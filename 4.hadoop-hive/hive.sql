f5931ffcefed71f318effd5140fc87d48617e233
dvstepanchuk <danylo.stepanchuk@globallogic.com>
2020-12-30 18:31:58 +0200

    GLR-HIVE: Added review comments

--- 4.hadoop-hive/hive.sql
+++ 4.hadoop-hive/hive.sql
@@ -16,7 +16,7 @@ DROP DATABASE IF EXISTS FlyLines CASCADE;
 CREATE DATABASE HadoopHive;
 USE HadoopHive;
 
-
+-- GLC| The first line in `flights` the is the header, isn't?
 CREATE EXTERNAL TABLE IF NOT EXISTS FlyLines
 (YEAR int, MONTH int, DAY int, DAY_OF_WEEK int, AIRLINE string,FLIGHT_NUMBER string, TAIL_NUMBER string, ORIGIN_AIRPORT string, DESTINATION_AIRPORT string, SCHEDULED_DEPARTURE string, DEPARTURE_TIME int,
  DEPARTURE_DELAY int,TAXI_OUT string, WHEELS_OFF string, SCHEDULED_TIME string, ELAPSED_TIME string, AIR_TIME string, DISTANCE string,
@@ -27,6 +27,7 @@ FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
 LOCATION 'hdfs:///bdpc/hive/flights';
 
+-- GLC| The first line in `airlines` the is the header, isn't?
 CREATE EXTERNAL TABLE IF NOT EXISTS Airlines(AIRLINE string, AIRNAME string);
 
 ROW FORMAT DELIMITED
@@ -34,11 +35,17 @@ FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
 LOCATION 'hdfs:///bdpc/hive/airlines';
 
-
+-- GLC| What about table creation?
+-- GLC| Is it the best solution in terms of execution performance? What alternatives could you think about?
+-- GLC| Recommend: Estimate execution performance with|w/o joining, review execution plan, play with execution engines
 SELECT F.AIRLINE, A.AIRNAME, AVG(F.DEPARTURE_DELAY) as AvgDelay
 FROM FlyLines F JOIN Airlines A ON F.AIRLINE = A.AIRLINE
 GROUP BY F.AIRLINE, A.AIRNAME
 ORDER BY AvgDelay DESC LIMIT 5;load script
+
+-- GLC| It seems a bit corrupted
+-- GLC| Any ideas on general testing solution?
+
 chmod +x
 #create dirs:
 hdfs dfs -mkdir -p /bdpc/hive/data/
